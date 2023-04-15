eebffe35aa4f260e6d53d9d091a294eaf0e242ad
Huệ Từ <tuluu.38@HT.local>
2023-04-15 21:18:51 +0700

    complet design home page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
-    if($(window).scrollTop()==0){
-        $(".header-section__header-top div").css("color", "white");
-        $(".navbar-list").css("color", "white");
-    }
-    if($(window).scrollTop()>0){
-        $(".header-section__header-top div").css("color", "black");
-        $(".navbar-list").css("color", "white");
-        $(".header-section__header-top").css("width", "100%");
-    }
+    $(window).scroll(function () { 
+        if($(window).scrollTop()==0){
+            $(".header-section__header-top div").css("color", "white");
+            $(".navbar-list").css("color", "white");
+            $(".button__go-top").css("display", "none");
+        }
+        if($(window).scrollTop()>0){
+            $(".header-section__header-top div").css("color", "black");
+            $(".navbar-list").css("color", "white");
+            $(".header-section__header-top").css("width", "100%");
+            $(".button__go-top").css("display", "block");
+        }
+    });
     $(".navbar-list__item").each(function(index, value){
         if(index < 5){
             $(this).hover(function (e) {
@@ -29,10 +33,44 @@ $(document).ready(function () {
     // service section animation
     // service-items animation
     gsap.fromTo(".service-item", {scale: 0, opacity: 0.1}, {scale:1, opacity:1, duration: 1.5});
-    // readmore button animation 
-    $(".service-item").hover(function(){
-        // gsap.fromTo((".button"), {width: "0px", opacity: 0.1, backgroundColor: "none"}, {width: "auto", opacity:1, backgroundColor: "white", duration: 1.5})
-        // console.log($(this).children($(".button")));
-    })
-    
+    // readmore button animation
+    // $(".block-main-content__project .grid-list").css({
+    //     "position" : "relative",
+    //     "display" : "flex",
+    //     "flex-wrap": "wrap",
+    //     "flex-direction" : "row",
+    //     "width" : "100%",
+    //     "height" : "fit-content"
+    // })
+    // $(".block-main-content__project .grid-item").css({
+    //     "width" : "calc(100%/3)",
+    //     "height" : "350px",
+    //     "position" : "relative"
+
+    // })
+    // $(".block-main-content__project .s-row").css({
+    //     "width" : "100%",
+    //     "height" : "100%"
+    // })
+    // $(".block-main-content__project img").css({
+    //     "width" : "100%",
+    //     "height" : "100%",
+    //     "object-fit" : "contain"
+    // })
+    // $.getJSON("../data/data.json", function (data) {
+    //        for(let i of data){
+    //             for(let j of i.products){
+    //                 $(".block-main-content__project .grid-item").append(
+    //                     `
+    //                         <div class="m-row">
+    //                             <div class="s-row">
+    //                                 <img src="${j.img}" alt="${j.img}">
+    //                             </div>
+    //                         </div>
+    //                     `
+    //                 )
+    //             }
+    //        }
+    //     }
+    // );
 });
